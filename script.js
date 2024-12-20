2d2802302dce9a814458ffd7ad34bb10d1212240
Alquileres-San-Miguel-Del-Monte <pato.g.gonzalez04@gmail.com>
2024-12-20 14:31:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ let markedDates = {
   EL_ENCANTO: {},
 };
 let currentHouse = "LOS_CHICOS"; // Casa seleccionada por defecto
-let touchTimeout; // Para controlar el tiempo de toque
 
 // Cargar datos almacenados localmente
 if (localStorage.getItem("markedDates")) {
@@ -40,13 +39,14 @@ function renderCalendar() {
   calendar.innerHTML = "";
 
   // Agregar los días de la semana
-  const daysOfWeek = ["L", "M", "M", "J", "V", "S", "D"];
-  daysOfWeek.forEach((day) => {
-    const dayHeader = document.createElement("div");
-    dayHeader.className = "day-header"; // Clase para estilo
-    dayHeader.textContent = day;
-    calendar.appendChild(dayHeader);
-  });
+const daysOfWeek = ["L", "M", "M", "J", "V", "S", "D"];
+daysOfWeek.forEach((day) => {
+  const dayHeader = document.createElement("div");
+  dayHeader.className = "day-header"; // Clase para estilo
+  dayHeader.textContent = day;
+  calendar.appendChild(dayHeader);
+});
+
 
   // Agregar los días vacíos al principio
   for (let i = 0; i < firstDay; i++) {
@@ -67,25 +67,15 @@ function renderCalendar() {
       dateCell.classList.add("cross");
     }
 
-    // Agregar el evento de mantener presionado
-    dateCell.addEventListener("touchstart", (e) => {
-      e.preventDefault(); // Prevenir el evento por defecto de touch
-
-      // Iniciar el temporizador para detectar el tiempo de toque
-      touchTimeout = setTimeout(() => {
-        if (markedDates[currentHouse][dateKey]) {
-          delete markedDates[currentHouse][dateKey];
-        } else {
-          markedDates[currentHouse][dateKey] = true;
-        }
-        saveMarkedDates(); // Guardar cambios
-        renderCalendar(); // Actualizar calendario
-      }, 1000); // 1 segundo
-    });
-
-    // Cancelar el temporizador si el toque termina antes de 1 segundo
-    dateCell.addEventListener("touchend", () => {
-      clearTimeout(touchTimeout);
+    // Agregar el evento de doble clic
+    dateCell.addEventListener("dblclick", () => {
+      if (markedDates[currentHouse][dateKey]) {
+        delete markedDates[currentHouse][dateKey];
+      } else {
+        markedDates[currentHouse][dateKey] = true;
+      }
+      saveMarkedDates(); // Guardar cambios
+      renderCalendar(); // Actualizar calendario
     });
 
     calendar.appendChild(dateCell);
@@ -106,7 +96,7 @@ houseSelector.addEventListener("click", (e) => {
 // Función para resaltar la casa seleccionada
 function highlightSelectedHouse() {
   const houseButtons = houseSelector.querySelectorAll("button");
-
+  
   houseButtons.forEach((button) => {
     if (button.getAttribute("data-house") === currentHouse) {
       button.classList.add("selected");
